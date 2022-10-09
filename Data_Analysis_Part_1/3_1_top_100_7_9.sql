a0ca6aa8b12ecb7ba4dfb258248accc0d6a6367f
JANEY DONG <janeydong1129@gmail.com>
2022-10-09 18:13:16 -0400

    update files

---
+++ Data_Analysis_Part_1/3_1_top_100_7_9.sql
@@ -0,0 +1,10 @@
+CREATE VIEW top_100_7_9 AS
+SELECT *
+FROM complete_table
+WHERE EXTRACT (hour from date) >= 7 or EXTRACT (hour from date) < 9
+ORDER BY amount DESC
+LIMIT 100
+
+SELECT name, card, date, amount, merchant, category
+FROM top_100_7_9
+GROUP BY name, card, date, amount, merchant, category
