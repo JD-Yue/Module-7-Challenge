d5888793674eb04a356e78717129ac87e0a5f9bf
JANEY DONG <janeydong1129@gmail.com>
2022-10-09 18:09:55 -0400

    update files

--- Data_Analysis_Part_1/4_2_top_100_rest_of_days.sql
+++ Data_Analysis_Part_1/4_2_top_100_rest_of_days.sql
@@ -1,7 +1,7 @@
 CREATE VIEW top_100_rest_of_day AS
 SELECT *
 FROM complete_table
-WHERE EXTRACT (hour from date) < 7 or EXTRACT (hour from date) > 9
+WHERE EXTRACT (hour from date) < 7 or EXTRACT (hour from date) >= 9
 ORDER BY amount DESC
 LIMIT 100
 
